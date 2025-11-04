5e80486476211abd9caa02cf0fc722be1b8a2033
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-04 15:30:02 +0100

    Update `index.php` file to use custom PSR-4 autoloader + register namespaces

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+use Helpers\Psr4AutoloaderClass;
+
+// Create and register our autoloader
+require_once __DIR__ . '/Helpers/Psr4AutoloaderClass.php';
+$loader = new Psr4AutoloaderClass();
+$loader->register();
+
+// Register app namespace<>path bindings
+$loader->addNamespace('\\Helpers', __DIR__ . '/Helpers');
+
+// Register vendors namespace<>path bindings
+$loader->addNamespace('\\League\\Plates\\', __DIR__ . '/Vendor/Plates/src');
+
+echo 'Hello World!';
