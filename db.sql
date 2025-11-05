192046271efc2a0165a4996aadce822035ed83e9
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-05 15:26:21 +0100

    fix(db) : change characters Image URLs

--- db.sql
+++ db.sql
@@ -13,6 +13,6 @@ CREATE TABLE personnages (
 
 -- Insert sample character data
 INSERT INTO personnages (id, name, element, unit_class, origin, rarity, url_image) VALUES
-('690a21938f4e03.77345231', 'Aino', 'Hydro', 'Claymore', 'Nod-Krai', 4, 'https://static.wikia.nocookie.net/gensin-impact/images/b/ba/Character_Aino_Game.png/revision/latest?cb=20251012045702'),
-('690a219b956ed6.60563743', 'Innefa', 'Electro', 'Polearm', 'Nod-Krai', 5, 'https://static.wikia.nocookie.net/gensin-impact/images/0/03/Character_Ineffa_Game.png/revision/latest?cb=20250903053543'),
-('690a219c1293b4.38536441', 'Chongyun', 'Cryo', 'Claymore', 'Liyue', 4, 'https://static.wikia.nocookie.net/gensin-impact/images/8/85/Character_Chongyun_Game.png/revision/latest?cb=20241004004414');
+('690a21938f4e03.77345231', 'Aino', 'Hydro', 'Claymore', 'Nod-Krai', 4, 'https://static.wikia.nocookie.net/gensin-impact/images/a/a3/Aino_Icon.png'),
+('690a219b956ed6.60563743', 'Innefa', 'Electro', 'Polearm', 'Nod-Krai', 5, 'https://static.wikia.nocookie.net/gensin-impact/images/0/0b/Ineffa_Icon.png'),
+('690a219c1293b4.38536441', 'Chongyun', 'Cryo', 'Claymore', 'Liyue', 4, 'https://static.wikia.nocookie.net/gensin-impact/images/3/35/Chongyun_Icon.png');
