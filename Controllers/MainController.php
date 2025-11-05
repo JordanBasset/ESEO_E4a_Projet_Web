6746a496c1922bee4ca8d9a0c4970a5ebb9d3bb6
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-05 14:35:02 +0100

    test that `PersonnageDAO` is working

--- Controllers/MainController.php
+++ Controllers/MainController.php
@@ -8,8 +8,18 @@ final readonly class MainController {
 	public function __construct(private Engine $plates) {}
 
 	public function index(): void {
+		// Test of the newly created DAO class
+		$dao = new \Models\PersonnageDAO();
+		$characters = $dao->getAll();
+		$firstCharacter = $dao->getByID('690a21938f4e03.77345231');
+		$nullCharacter = $dao->getByID('690a21938f4e04.77345232');
+
 		echo $this->plates->render('home', [
 			'gameName' => 'Genshin Impact',
+			// Inject the test variables into the template
+			'characters' => $characters,
+			'firstCharacter' => $firstCharacter,
+			'nullCharacter' => $nullCharacter,
 		]);
 	}
 }
--- Views/home.php
+++ Views/home.php
@@ -2,3 +2,9 @@
 $this->layout('template', ['title' => 'TP Mihoyo']);
 ?>
 <h1>Collection <?= $this->e($gameName) ?></h1>
+
+<pre>
+	<?php var_dump($characters); ?>
+	<?php var_dump($firstCharacter); ?>
+	<?php var_dump($nullCharacter); ?>
+</pre>
