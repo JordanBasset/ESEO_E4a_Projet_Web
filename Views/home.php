4ae66a2fc01f57f20b063149ecac17f1052a618e
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-05 15:27:17 +0100

    feat : Render characters into homepage
    add Bootstrap to facilitate website design

--- Views/home.php
+++ Views/home.php
@@ -3,8 +3,13 @@ $this->layout('template', ['title' => 'TP Mihoyo']);
 ?>
 <h1>Collection <?= $this->e($gameName) ?></h1>
 
-<pre>
-	<?php var_dump($characters); ?>
-	<?php var_dump($firstCharacter); ?>
-	<?php var_dump($nullCharacter); ?>
-</pre>
+<div class="row row-cols-4" id="characters-list">
+	<?php foreach ($characters as $character): ?>
+		<div class="character"
+			 data-character-id="<?= $character->id ?>"
+			 data-character-rarity="<?= $character->rarity ?>">
+			<img src="<?= $character->urlImg ?>" alt="<?= $character->name ?>"/>
+			<p class="character-name fs-3"><?= $character->name ?></p>
+		</div>
+	<?php endforeach; ?>
+</div>
