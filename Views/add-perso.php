edd440fe35973e5f01e0c0d93d8044a2cb6b0ac6
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-07 14:13:51 +0100

    feat : add "Add Character" and "Add Element" forms
    fix main stylesheet position in `head` page tag

--- Views/add-perso.php
+++ Views/add-perso.php
@@ -2,3 +2,20 @@
 $this->layout('template', ['title' => 'TP Mihoyo']);
 ?>
 <h1>Add Character</h1>
+
+<fieldset class="form-container">
+	<form action="/?action=add-character" method="post" autocomplete="off">
+		<input type="text" class="form-control" name="name" placeholder="Name"><br>
+		<select class="form-select" name="element">
+			<option value="" disabled selected>Element</option>
+			<option value="hydro">Hydro</option>
+			<option value="electro">Electro</option>
+			<option value="cryo">Cryo</option>
+		</select><br>
+		<input type="text" class="form-control" name="unit_class" placeholder="Weapon"><br>
+		<input type="number" class="form-control" name="rarity" min="3" max="5" step="1" placeholder="Quality"><br>
+		<input type="text" class="form-control" name="origin" placeholder="Region"><br>
+		<input type="url" class="form-control" name="image_url" placeholder="Image URL"><br>
+		<input type="submit" class="btn btn-outline-primary w-100" name="submit" value="Add Character">
+	</form>
+</fieldset>
--- Views/template.php
+++ Views/template.php
@@ -2,11 +2,11 @@
 <html lang="fr">
 	<head>
 		<meta charset="UTF-8"/>
-		<link rel="stylesheet" href="/public/css/main.css"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
 			  integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="/public/css/main.css"/>
 		<title><?= $this->e($title) ?></title>
 	</head>
 	<body>
