4305a6bb3821c3847dffe3b6f5071ae99ada2424
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-25 16:49:00 +0100

    fix "Add Element" form submit action name

--- Views/add-element.php
+++ Views/add-element.php
@@ -4,7 +4,7 @@ $this->layout('template', ['title' => 'TP Mihoyo']);
 <h1>Add Element</h1>
 
 <fieldset class="form-container">
-	<form action="/?action=add-character-element" method="post" autocomplete="off">
+	<form action="/?action=add-element" method="post" autocomplete="off">
 		<input type="text" class="form-control" name="name" placeholder="Name"><br>
 		<input type="url" class="form-control" name="image_url" placeholder="Image URL"><br>
 		<input type="submit" class="btn btn-outline-primary w-100" name="submit" value="Add Element">
