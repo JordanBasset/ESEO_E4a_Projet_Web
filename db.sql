8b6b6ee088df638e595d9fe265e0b736c00bdd5f
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-25 15:08:03 +0100

    fix character name typo

--- db.sql
+++ db.sql
@@ -14,5 +14,5 @@ CREATE TABLE personnages (
 -- Insert sample character data
 INSERT INTO personnages (id, name, element, unit_class, origin, rarity, url_image) VALUES
 ('690a21938f4e03.77345231', 'Aino', 'Hydro', 'Claymore', 'Nod-Krai', 4, 'https://static.wikia.nocookie.net/gensin-impact/images/a/a3/Aino_Icon.png'),
-('690a219b956ed6.60563743', 'Innefa', 'Electro', 'Polearm', 'Nod-Krai', 5, 'https://static.wikia.nocookie.net/gensin-impact/images/0/0b/Ineffa_Icon.png'),
+('690a219b956ed6.60563743', 'Ineffa', 'Electro', 'Polearm', 'Nod-Krai', 5, 'https://static.wikia.nocookie.net/gensin-impact/images/0/0b/Ineffa_Icon.png'),
 ('690a219c1293b4.38536441', 'Chongyun', 'Cryo', 'Claymore', 'Liyue', 4, 'https://static.wikia.nocookie.net/gensin-impact/images/3/35/Chongyun_Icon.png');
