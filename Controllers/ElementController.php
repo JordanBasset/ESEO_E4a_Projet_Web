af5eee2e82a85af61c3987495222696cad7c2004
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-25 16:55:36 +0100

    feat : adapt "Add Element" form to add/edit action for each game data type

--- Controllers/ElementController.php
+++ Controllers/ElementController.php
@@ -7,7 +7,12 @@ use League\Plates\Engine;
 final readonly class ElementController {
 	public function __construct(private Engine $plates) {}
 
-	public function displayAddElement(): void {
-		echo $this->plates->render('add-element');
+	public function displayAddElement(?string $errorMessage = null): void {
+		$viewData = $errorMessage ? ['error' => $errorMessage] : [];
+		echo $this->plates->render('add-element', [
+			'editMode' => false,
+			'editType' => 'element',
+			...$viewData,
+		]);
 	}
 }
--- Views/add-element.php
+++ Views/add-element.php
@@ -1,12 +1,22 @@
 <?php
 $this->layout('template', ['title' => 'TP Mihoyo']);
+
+$actionType = $editMode ? 'edit' : 'add';
 ?>
-<h1>Add Element</h1>
+
+<h1><?= ucwords($actionType) ?> <?= ucwords($editType) ?></h1>
+
+<?php
+if (!empty($error)):
+	$this->insert('alert', ['message' => $error, 'type' => 'danger']);
+endif;
+?>
 
 <fieldset class="form-container">
-	<form action="/?action=add-element" method="post" autocomplete="off">
+	<form action="/?action=<?= $actionType ?>-<?= $editType ?>" method="post" autocomplete="off">
 		<input type="text" class="form-control" name="name" placeholder="Name"><br>
 		<input type="url" class="form-control" name="image_url" placeholder="Image URL"><br>
-		<input type="submit" class="btn btn-outline-primary w-100" name="submit" value="Add Element">
+		<input type="submit" class="btn btn-outline-primary w-100" name="submit"
+			   value="<?= ucwords($actionType) ?> <?= ucwords($editType) ?>">
 	</form>
 </fieldset>
