b414ffbb0add514fa0bb4ef603e11bb72f4621e9
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-26 14:30:41 +0100

    refactor : remove useless variables / comments

--- Controllers/MainController.php
+++ Controllers/MainController.php
@@ -26,21 +26,15 @@ final readonly class MainController {
 	 * @param ?string $messageType Type for the optional message to display on the view
 	 */
 	public function index(?string $message = null, ?string $messageType = null): void {
-		// Test of the newly created DAO class
 		$service = new PersonnageService(new PersonnageDAO());
 		$characters = $service->getAll();
-		$firstCharacter = $service->getByID('690a21938f4e03.77345231');
-		$nullCharacter = $service->getByID('690a21938f4e04.77345232');
 
 		$alertData = $message && $messageType ? ['message' => $message, 'type' => $messageType] : [];
 
 		echo $this->plates->render('home', [
 			'gameName' => 'Genshin Impact',
 			'alert' => $alertData,
-			// Inject the test variables into the template
 			'characters' => $characters,
-			'firstCharacter' => $firstCharacter,
-			'nullCharacter' => $nullCharacter,
 		]);
 	}
 
