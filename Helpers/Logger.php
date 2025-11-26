be02f5234f618a68e30098a847e21a4ff041dbda
JordanBasset <104905185+JordanBasset@users.noreply.github.com>
2025-11-26 13:49:04 +0100

    refactor : Add `Logger` class documentation

--- Helpers/Logger.php
+++ Helpers/Logger.php
@@ -2,12 +2,28 @@
 
 namespace Helpers;
 
+/**
+ * Class that handles reading and writing logs.
+ */
 readonly class Logger {
 	private const string LOG_FILE_FORMAT = 'mihoyo-%04d-%02d.log';
 
+	/**
+	 * Creates a new logger instance.
+	 *
+	 * @param string $logDir Folder where logs are stored.
+	 */
 	public function __construct(private string $logDir) {
 	}
 
+	/**
+	 * Writes a new log entry.
+	 *
+	 * @param bool $success If the action performed was successful or not
+	 * @param string $actionType The kind of action that was performed
+	 * @param string $entity The entity class name that was affected
+	 * @param string $message The message to log
+	 */
 	public function log(bool $success, string $actionType, string $entity, string $message): void {
 		$logFile = sprintf(self::LOG_FILE_FORMAT, date('Y'), date('m'));
 		$logPath = $this->logDir . DIRECTORY_SEPARATOR . $logFile;
@@ -24,6 +40,11 @@ readonly class Logger {
 		}
 	}
 
+	/**
+	 * Returns a list of the files in the log folder.
+	 *
+	 * @return string[] List of log files
+	 */
 	public function getLogFiles(): array {
 		if (!file_exists($this->logDir)) {
 			return [];
@@ -33,6 +54,12 @@ readonly class Logger {
 		return array_filter($files, static fn($file) => str_ends_with($file, '.log'));
 	}
 
+	/**
+	 * Returns the content of a log file.
+	 *
+	 * @param string $logFile Log file name
+	 * @return string[] Log file contents, one array item per log file line
+	 */
 	public function getLogFileContent(string $logFile): array {
 		if (!file_exists($this->logDir . DIRECTORY_SEPARATOR . $logFile)) {
 			return [];
